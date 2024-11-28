d32f4529cf39e02878294ee7cfbd857c3a799312
mirekondro1 <130686217+mirekondro@users.noreply.github.com>
2024-11-28 19:09:05 +0100

    feat: gsap implementation

--- src/app/page.js
+++ src/app/page.js
@@ -6,18 +6,23 @@ import EmailSection from "./components/EmailSection";
 import Footer from "./components/Footer";
 import AchievementsSection from "./components/AchievementsSection";
 
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
       <Navbar />
+    <div id="smooth-wrapper">
+    <div id="smooth-content">
       <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
         <AchievementsSection />
         <AboutSection />
         <ProjectsSection />
-        <EmailSection />
-      </div>
+        <EmailSection />      
       <Footer />
+      </div>
+      </div>
+  </div>
     </main>
   );
 }
