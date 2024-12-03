7834dad073ccea27632d5b0a27e528414534a907
mirekondro1 <130686217+mirekondro@users.noreply.github.com>
2024-12-03 23:19:26 +0100

    feat: gasp animation homepage

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,25 +1,36 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import Image from "next/image";
 import { TypeAnimation } from "react-type-animation";
-import { motion } from "framer-motion";
+import { gsap } from "gsap";
 import Link from "next/link";
 
 const HeroSection = () => {
+  useEffect(() => {
+    gsap.fromTo(
+      ".hero-image",
+      { opacity: 0, x: 200 },
+      { opacity: 1, x: 0, duration: 1 }
+    );
+  }, []);
+
+  const handleMouseEnter = (e) => {
+    gsap.to(e.target, { scale: 1.1, duration: 0.3 });
+  };
+
+  const handleMouseLeave = (e) => {
+    gsap.to(e.target, { scale: 1, duration: 0.3 });
+  };
+
   return (
     <section className="lg:py-16">
       <div className="grid grid-cols-1 sm:grid-cols-12">
-        <motion.div
-          initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.5 }}
-          className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
-        >
+        <div className="col-span-8 place-self-center text-center sm:text-left justify-self-start">
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
               Hello, I&apos;m{" "}
             </span>
-            <br></br>
+            <br />
             <TypeAnimation
               sequence={[
                 "Miroslav",
@@ -56,23 +67,21 @@ const HeroSection = () => {
               </span>
             </Link>
           </div>
-        </motion.div>
-        <motion.div
-          initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.5 }}
-          className="col-span-4 place-self-center mt-4 lg:mt-0"
-        >
+        </div>
+
+        <div className="col-span-4 place-self-center mt-4 lg:mt-0">
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
               src="/images/illustration.png"
               alt="hero image"
-              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              className="hero-image absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={300}
               height={300}
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
             />
           </div>
-        </motion.div>
+        </div>
       </div>
     </section>
   );
